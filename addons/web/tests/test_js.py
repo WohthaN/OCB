47ba6254a9d15c3902927d56859a2caed7fbfd73
Samuele Carli <carlisamuele@csspace.net>
2018-02-27 20:15:56 +0100

    [ZELO][FIX] web: unit test clear inheriting templates
    * Web unit tests did a reset on web.layout which in turn
      broke if some other module wound inherit from it.
      Now the unit tests modifies all inheriting templates as well.

--- addons/web/tests/test_js.py
+++ addons/web/tests/test_js.py
@@ -2,12 +2,13 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 import re
+
 import odoo.tests
 
 RE_ONLY = re.compile('QUnit\.only\(')
 
-class WebSuite(odoo.tests.HttpCase):
 
+class WebSuite(odoo.tests.HttpCase):
     post_install = True
     at_install = False
 
@@ -26,8 +27,27 @@ class WebSuite(odoo.tests.HttpCase):
 
     def _check_only_call(self, suite):
         # As we currently aren't in a request context, we can't render `web.layout`.
-        # redefinied it as a minimal proxy template.
-        self.env.ref('web.layout').write({'arch_db': '<t t-name="web.layout"><t t-raw="head"/></t>'})
+        # redefinied it as a minimal proxy template. To avoid reference errors, any template
+        # depending on it is minimized as well.
+
+        minimal_template = """
+        <t t-name="web.layout">
+            <t t-raw="head"/>
+        </t>
+        """
+        View_model = self.env['ir.ui.view']
+
+        web_layout_view = self.env.ref('web.layout')
+
+        inheriting_views = web_layout_view.get_inheriting_views_arch(web_layout_view.ids[0], None)
+        inheriting_views = [x[1] for x in
+                            inheriting_views]  # get_inheriting_views_arch returns ["view content", view_id)]
+
+        for view_id in inheriting_views:
+            view = View_model.browse(view_id)
+            view.write({'arch_db': minimal_template})
+
+        web_layout_view.write({'arch_db': minimal_template})
 
         for asset in self.env['ir.qweb']._get_asset_content(suite, options={})[0]:
             filename = asset['filename']
